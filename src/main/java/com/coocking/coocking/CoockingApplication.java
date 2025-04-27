a543c59452a699f3d49ffe3423c7d07fc3b606ae
Santiago <sbenitez1607@gmail.com>
2025-04-26 23:10:10 -0500

    First commit

---
+++ src/main/java/com/coocking/coocking/CoockingApplication.java
@@ -0,0 +1,13 @@
+package com.coocking.coocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoockingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoockingApplication.class, args);
+	}
+
+}
